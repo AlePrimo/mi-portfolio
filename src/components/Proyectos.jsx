4b551035d86e2ae8161f1ba286d65f03d30c5670
AlePrimo <alejandrojuliancarullo@hotmail.com>
2025-06-02 20:06:52 -0300

    Modificacion de Proyectos.jsx

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -6,7 +6,7 @@ function Proyectos() {
     <section id="proyectos">
       <h2>Proyectos</h2>
       <ul>
-        <li><strong>JobFlowApp</strong>: Plataforma para gestionar búsqueda de empleo (Spring Boot + OAuth + MySQL)</li>
+        <li><strong>JobFlowApp</strong>: Plataforma para gestionar búsqueda de empleo (Spring Boot + MySQL + Postman)</li>
         <li><em>Otros proyectos próximamente...</em></li>
       </ul>
     </section>
